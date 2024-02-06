75355eaf1b138ab5c17113a9a304c2a39a09e2ac
illaolioa <frantisekboldis@gmail.com>
2024-02-06 13:22:43 +0100

    react native side made prettier adding animations and icons

--- App.jsx
+++ App.jsx
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, StyleSheet, NativeModules, Button, FlatList } from 'react-native';
+import { View, TextInput, StyleSheet, NativeModules, Button, FlatList,LayoutAnimation,
+  Platform,UIManager } from 'react-native';
 import TodoRender from './components/TodoRender';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const SharedStorage = NativeModules.SharedStorage;
+
+if (
+  Platform.OS === 'android' &&
+  UIManager.setLayoutAnimationEnabledExperimental
+) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 
 const App = () => {
   const [text, setText] = useState('');
@@ -44,14 +53,18 @@ const App = () => {
   };
 
   const removeTodo = (index) => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
     const updatedTodos = [...todos.slice(0, index), ...todos.slice(index + 1)];
     setTodos(updatedTodos);
   };
   const AddTodo = () => {
-    setTodos((prevTodos) => [...prevTodos, text]);
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+    text !== "" ? setTodos((prevTodos) => [...prevTodos, text]) : null;
+
     setText(''); 
   };
   const reorderTodo = (currentIndex, targetIndex) => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
     if (currentIndex < 0 || currentIndex >= todos.length || targetIndex < 0 || targetIndex >= todos.length) {
       return;
     }
@@ -70,25 +83,25 @@ const App = () => {
      <View className="flex-1"> 
       <FlatList
             className="flex-grow-0"
+              
               data={todos}
+              
               renderItem={({ item, index }) => <TodoRender cont={item} index={index} removeTodo={removeTodo} reorderTodo={reorderTodo} />}
             />
     </View> 
     
-    <View className="items-center justify-evenly bg-slate-900 h-2/6" > 
+    <View className="items-center justify-evenly bg-slate-900 h-1/5" > 
       <TextInput 
           style={styles.input}
           onChangeText={(newText) => setText(newText)}
           value={text}
           returnKeyType="send"
-          placeholder="Enter the text to display..."
+          placeholder="Sem píšte"
       />
-      <Button onPress={AddTodo} title="Pridať!" />
+      
+      <Icon.Button backgroundColor="transparent" size={50} name="pluscircleo" onPress={AddTodo} />
     </View> 
-      
-      
     </View>
-  
   );
 };
 
@@ -96,9 +109,11 @@ export default App;
 
 const styles = StyleSheet.create({
   input: {
-    width: '100%',
+    width: '90%',
     borderBottomWidth: 1,
+    borderRadius:15,
     fontSize: 20,
     minHeight: 40,
+    backgroundColor:"purple"
   },
 });
--- components/TodoRender.js
+++ components/TodoRender.js
@@ -1,16 +1,27 @@
-import { View,Text, Button } from "react-native"
+import { View,Text } from "react-native"
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const TodoRender = ({cont,index,removeTodo,reorderTodo}) => {
-    
+    const generateRandomColor = () => {
+        const baseColor = [30, 16, 23]; 
+        const randomHue = baseColor[0] + cont.charCodeAt(0) - 102;
+        const randomSaturation = baseColor[1] + cont.charCodeAt(0) - 120;
+        const randomLightness = baseColor[2] + cont.charCodeAt(0) - 100;
+
+        return `hsl(${randomHue}, ${randomSaturation}%, ${randomLightness}%)`;
+    };
+
+    const backgroundColor = generateRandomColor();
     return(
-         <View className="flex-row items-center justify-evenly">
-             <Text>
+         <View className="flex-row items-center justify-evenly" style={{backgroundColor:backgroundColor}}>
+          <View> 
+          <Icon.Button backgroundColor="#1E293B" name="arrowup" onPress={()=>reorderTodo(index,index-1)}></Icon.Button> 
+            <Icon.Button backgroundColor="#1E293B" name="arrowdown" onPress={()=>reorderTodo(index,index+1)}></Icon.Button> 
+         </View> 
+         <Text className="flex-1 pl-3">
                 {cont}
-            </Text>  
-             <Button title="zmazat!" onPress={()=>removeTodo(index)}>
-            </Button> 
-            <Button title="^" onPress={()=>reorderTodo(index,index-1)}></Button> 
-            <Button title="\/" onPress={()=>reorderTodo(index,index+1)}></Button> 
+            </Text>       
+            <Icon.Button backgroundColor="transparent" name="closecircleo" onPress={()=>removeTodo(index)}></Icon.Button> 
         </View> 
     )
 }
